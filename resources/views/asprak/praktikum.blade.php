24d2829f6fa9fb85a5b8f5cd1869c35a7a944e82
FuadFikri <fuadfikri01@gmail.com>
2018-11-09 16:35:56 +0700

    kelas

--- resources/views/asprak/praktikum.blade.php
+++ resources/views/asprak/praktikum.blade.php
@@ -13,7 +13,7 @@
               <div class="col-md-12 col-sm-12 col-xs-12">
                 <div class="x_panel">
                   <div class="x_title">
-                  <h2>{{ $praktikum->nama }}</h2>{{$data}}
+                  <h2>{{ $praktikum->nama }} <b>{{$praktikum->kelas}}</b> </h2>
 
                     <div class="clearfix"></div>
                 </div>
