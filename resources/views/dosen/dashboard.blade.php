fa80dd81794c5ce4a7de38d1dcdd5fb47254c7b5
FuadFikri <fuadfikri01@gmail.com>
2018-12-13 09:30:57 +0700

    bersih-bersih fronted

--- resources/views/dosen/dashboard.blade.php
+++ resources/views/dosen/dashboard.blade.php
@@ -13,9 +13,9 @@
 
             <!-- menu profile quick info -->
             <div class="profile clearfix">
-              <div class="profile_pic">
+              {{-- <div class="profile_pic">
                 <img src="{{ asset('assets/images/img.jpg') }}" alt="..." class="img-circle profile_img">
-              </div>
+              </div> --}}
               <div class="profile_info">
                 <span>Welcome,</span>
                 <h2>John Doe</h2>
